3bdeffb23442b3fbd19fc9964c6d48f581f6a16a
Monzurul Islam <Xatta-Trone@users.noreply.github.com>
2022-10-10 05:35:58 +0900

    update 10 oct

--- dist/_nuxt/83a3610.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[61],{636:function(t,e,n){"use strict";n.r(e);var o={layout:"app",components:{postadd:n(576).default}},l=n(11),component=Object(l.a)(o,(function(){var t=this.$createElement;return(this._self._c||t)("postadd",{attrs:{post_type:"software"}})}),[],!1,null,null,null);e.default=component.exports}}]);
